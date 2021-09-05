f60e5e493b16334b460b2517053e29b769772a71
HelloMaggieP <m.pentcheva@hotmail.com>
2021-09-05 14:16:52 +0100

    can display a single quote on page

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 const Quote = ({ronQuote, donaldQuote})=> {
     
-    const handleDisplay = ()=>{
-        if (ronQuote == )
-    }
-
+    
     return(
         <>
-        <h2>{ronQuote}</h2>
+        <h2>{ronQuote.text}</h2>
         <h2>{donaldQuote}</h2>
         </>
     )
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -4,46 +4,49 @@ import Quote from '../components/Quote';
 const Game = ()=>{
     const [ronQuote, setRonQuote] = useState({text: ""})
     const [donaldQuote, setDonaldQuote] = useState([])
+    //Q:Not sure why Donald Quote takes [] and Ron is ""
    
     useEffect(()=>{
-        return (ronQuote)
+        return console.log(ronQuote)
     },[ronQuote])
 
     useEffect(()=>{
-        return (donaldQuote)
+        return console.log(donaldQuote)
     },[donaldQuote])
 
+    //fetch requests to APIs
     const fetchRonQuote = function(){
+        console.log("Ron Function Called")
         fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
         .then(res => res.json())
         .then(ronQuote => setRonQuote({text: ronQuote}))
-        return ronQuote
     }
     const fetchDonaldQuote = function(){
+        console.log("Donald Function Called")
         fetch("https://api.tronalddump.io/random/quote")
         .then(res => res.json())
         .then(donaldQuote => setDonaldQuote(donaldQuote.value))
     }
 
-    // const handleClickNewQuote = ()=>{
-    //     const option = Math.floor(Math.random() * 2);
-    //     const options = [fetchRonQuote, fetchDonaldQuote]
-    //     if (option === 0){
-    //         return options[0];
-    //     }else{
-    //         return options[1];
-    //     }  
-    // }
+    //random choice generator between which API to send a fetch request to
+    const handleClickNewQuote = ()=>{
+        const option = Math.floor(Math.random() * 2);
+        const options = [fetchRonQuote, fetchDonaldQuote]
+            if (option === 0){
+                return options[0]();
+            }else{
+                return options[1]();
+            }     
+    }
     
     return (
         <>
             <h1>Who Said This?</h1>
-            {/* <button onClick={handleClickNewQuote}>Get New Quote</button> */}
+            <button onClick={handleClickNewQuote} >Get New Quote</button>
                
-            <Quote ronQuote = {ronQuote.text} donaldQuote = {donaldQuote} />
-        
-            <button onClick={fetchRonQuote}>Ron Quote</button>
-            <button onClick={fetchDonaldQuote}>Donald Quote</button>
+            <Quote ronQuote = {ronQuote} donaldQuote = {donaldQuote}/>
+            {/* <button onClick={fetchRonQuote}>Ron Quote</button>
+            <button onClick={fetchDonaldQuote}>Donald Quote</button> */}
         </>
     )
 }
